6cf8e1992e6ae2cdba80afe758d72103d838681a
Jesse Adelman <jesse@boldandbusted.com>
2018-04-17 13:10:16 -0400

    Move variables to proper modules, set enable_EC2_IAM_Auth on vault-cluster-private example.

--- examples/vault-cluster-private/main.tf
+++ examples/vault-cluster-private/main.tf
@@ -39,6 +39,7 @@ module "vault_cluster" {
   allowed_inbound_cidr_blocks        = ["0.0.0.0/0"]
   allowed_inbound_security_group_ids = []
   ssh_key_name                       = "${var.ssh_key_name}"
+  enable_EC2_IAM_Auth                = true
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
--- variables.tf
+++ variables.tf
@@ -97,18 +97,8 @@ variable "force_destroy_s3_bucket" {
   default     = false
 }
 
-variable "vpc_id" {
-    description = "The ID of the VPC in which the nodes will be deployed.  Uses default VPC if not supplied."
-    default = ""
-}
-
 variable "enable_s3_backend" {
   description = "Whether to configure an S3 storage backend in addition to Consul."
-  default     = false
-}
-
-variable "enable_EC2_IAM_Auth" {
-  description = "Configure IAM Instance Profile on Vault cluster members to permit the user to enable AWS Auth backend. Note that this does NOT actually enable the backend, but merely sets policys that will permit it to function as expected."
   default     = false
 }
 

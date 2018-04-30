fdb90f1f8d0b77aa8e0a15198ce50d21c5831355
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-30 02:18:35 +0100

    Update to newest Terratest

--- test/vault_cluster_s3_backend_test.go
+++ test/vault_cluster_s3_backend_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestVaultClusterS3BackendWithUbuntuAmi(t *testing.T) {
     t.Parallel()
-    runVaultWithS3BackendClusterTest(t, "vault-public-s3-ubuntu", "ubuntu16-ami", "ubuntu")
+    runVaultWithS3BackendClusterTest(t, "ubuntu16-ami", "ubuntu")
 }
 
 func TestVaultClusterS3BackendAmazonLinuxAmi(t *testing.T) {
     t.Parallel()
-    runVaultWithS3BackendClusterTest(t, "vault-public-s3-amzn", "amazon-linux-ami", "ec2-user")
+    runVaultWithS3BackendClusterTest(t, "amazon-linux-ami", "ec2-user")
 }

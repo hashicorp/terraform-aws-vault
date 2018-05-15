669e598961ecee955c8bd3d0ac790143b11f0fd9
Alex Tamoykin <alex@rightbalance.io>
2018-05-14 18:23:39 -0700

    Take group count from the variable

--- modules/vault-security-group-rules/main.tf
+++ modules/vault-security-group-rules/main.tf
@@ -2,6 +2,11 @@
 # CREATE THE SECURITY GROUP RULES THAT CONTROL WHAT TRAFFIC CAN GO IN AND OUT OF A VAULT CLUSTER
 # ---------------------------------------------------------------------------------------------------------------------
 
+variable "group_count" {
+}
+
+group_count = "${length(var.allowed_inbound_security_group_ids)}"
+
 resource "aws_security_group_rule" "allow_api_inbound_from_cidr_blocks" {
   count       = "${length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0}"
   type        = "ingress"
@@ -14,7 +19,7 @@ resource "aws_security_group_rule" "allow_api_inbound_from_cidr_blocks" {
 }
 
 resource "aws_security_group_rule" "allow_api_inbound_from_security_group_ids" {
-  count                    = "${length(var.allowed_inbound_security_group_ids)}"
+  count                    = "${var.group_count}"
   type                     = "ingress"
   from_port                = "${var.api_port}"
   to_port                  = "${var.api_port}"

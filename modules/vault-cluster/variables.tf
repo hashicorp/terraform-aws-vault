0761984599277f1f8f470d926cc6de3d0e754145
Joshua D Wells <Joshua_D_Wells@progressive.com>
2018-07-05 13:22:30 -0400

    adding vault dynamo variables (read/write and name)

--- modules/vault-cluster/variables.tf
+++ modules/vault-cluster/variables.tf
@@ -176,12 +176,32 @@ variable "enable_s3_backend" {
   default     = false
 }
 
+variable "enable_dynamo_backend" {
+  description = "Whether to configure a DynamoDB storage backend (No Consul)"
+  default     = false
+}
+
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket to create and use as a storage backend. Only used if 'enable_s3_backend' is set to true."
+  default     = ""
+}
+
+variable "dynamo_table_name" {
+  description = "The name of the Dynamo Table to create and use as a storage backend. Only used if 'enable_dynamo_backend' is set to true."
   default     = ""
 }
 
 variable "force_destroy_s3_bucket" {
   description = "If 'configure_s3_backend' is enabled and you set this to true, when you run terraform destroy, this tells Terraform to delete all the objects in the S3 bucket used for backend storage. You should NOT set this to true in production or you risk losing all your data! This property is only here so automated tests of this module can clean up after themselves. Only used if 'enable_s3_backend' is set to true."
   default     = false
+}
+
+variable "dynamo_read_capacity" {
+  description = "Sets the DynamoDB read capacity for storage backend"
+  default     = "5"
+}
+
+variable "dynamo_write_capacity" {
+  description = "Sets the DynamoDB write capacity for storage backend"
+  default     = "5"
 }

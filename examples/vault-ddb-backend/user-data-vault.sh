011d71a7ce49fa10f0f246be43bb3f6211d23a08
Joshua Wells <joshuawells@Joshuas-MacBook-Pro.local>
2018-07-06 08:02:13 -0400

    update userdata comment

--- examples/vault-ddb-backend/user-data-vault.sh
+++ examples/vault-ddb-backend/user-data-vault.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is meant to be run in the User Data of each EC2 Instance while it's booting. The script uses the
-# run-consul script to configure and start Consul in client mode and then the run-vault script to configure and start
+# run-vault script to configure and start
 # Vault in server mode. Note that this script assumes it's running in an AMI built from the Packer template in
 # examples/vault-consul-ami/vault-consul.json.
 

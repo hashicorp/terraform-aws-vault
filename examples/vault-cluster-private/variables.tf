bbdf145ec7a5ec0e9344df538dd8e7f0959ebdc0
Matt Calhoun <matt@mattcalhoun.com>
2019-06-27 11:00:12 -0400

    feat: upgrade example vault-cluster-private to tf12

--- examples/vault-cluster-private/variables.tf
+++ examples/vault-cluster-private/variables.tf
@@ -14,10 +14,12 @@
 
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/vault-consul-ami/vault-consul.json."
+  type        = string
 }
 
 variable "ssh_key_name" {
   description = "The name of an EC2 Key Pair that can be used to SSH to the EC2 Instances in this cluster. Set to an empty string to not associate a Key Pair."
+  type        = string
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -27,40 +29,49 @@ variable "ssh_key_name" {
 
 variable "vault_cluster_name" {
   description = "What to name the Vault server cluster and all of its associated resources"
+  type        = string
   default     = "vault-example"
 }
 
 variable "consul_cluster_name" {
   description = "What to name the Consul server cluster and all of its associated resources"
+  type        = string
   default     = "consul-example"
 }
 
 variable "vault_cluster_size" {
   description = "The number of Vault server nodes to deploy. We strongly recommend using 3 or 5."
+  type        = number
   default     = 3
 }
 
 variable "consul_cluster_size" {
   description = "The number of Consul server nodes to deploy. We strongly recommend using 3 or 5."
+  type        = number
   default     = 3
 }
 
 variable "vault_instance_type" {
   description = "The type of EC2 Instance to run in the Vault ASG"
+  type        = string
   default     = "t2.micro"
 }
 
 variable "consul_instance_type" {
   description = "The type of EC2 Instance to run in the Consul ASG"
+  type        = string
   default     = "t2.nano"
 }
 
 variable "consul_cluster_tag_key" {
   description = "The tag the Consul EC2 Instances will look for to automatically discover each other and form a cluster."
+  type        = string
   default     = "consul-servers"
 }
 
 variable "vpc_id" {
   description = "The ID of the VPC to deploy into. Leave an empty string to use the Default VPC in this region."
-  default     = ""
+  type        = string
+  default     = null
 }
+

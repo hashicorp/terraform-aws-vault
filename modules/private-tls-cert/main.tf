f0e7d8863ba1fc0ab0f67c9c46bc07a98ca3b0c8
Matt Calhoun <matt@mattcalhoun.com>
2019-06-27 10:35:23 -0400

    docs: remove double comments

--- modules/private-tls-cert/main.tf
+++ modules/private-tls-cert/main.tf
@@ -22,7 +22,6 @@ resource "tls_self_signed_cert" "ca" {
   }
 
   # Store the CA public key in a file.
-  # Store the CA public key in a file.
   provisioner "local-exec" {
     command = "echo '${tls_self_signed_cert.ca.cert_pem}' > '${var.ca_public_key_file_path}' && chmod ${var.permissions} '${var.ca_public_key_file_path}' && chown ${var.owner} '${var.ca_public_key_file_path}'"
   }
@@ -37,7 +36,6 @@ resource "tls_private_key" "cert" {
   ecdsa_curve = var.private_key_ecdsa_curve
   rsa_bits    = var.private_key_rsa_bits
 
-  # Store the certificate's private key in a file.
   # Store the certificate's private key in a file.
   provisioner "local-exec" {
     command = "echo '${tls_private_key.cert.private_key_pem}' > '${var.private_key_file_path}' && chmod ${var.permissions} '${var.private_key_file_path}' && chown ${var.owner} '${var.private_key_file_path}'"
@@ -67,7 +65,6 @@ resource "tls_locally_signed_cert" "cert" {
   validity_period_hours = var.validity_period_hours
   allowed_uses          = var.allowed_uses
 
-  # Store the certificate's public key in a file.
   # Store the certificate's public key in a file.
   provisioner "local-exec" {
     command = "echo '${tls_locally_signed_cert.cert.cert_pem}' > '${var.public_key_file_path}' && chmod ${var.permissions} '${var.public_key_file_path}' && chown ${var.owner} '${var.public_key_file_path}'"

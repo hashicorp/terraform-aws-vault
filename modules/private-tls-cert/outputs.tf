d7a56254ac3f627975ce3bdfde4d90911c9c9dcb
Teodor Linkin <teodor.linkin@technolink.org>
2019-07-01 13:05:22 +0300

    fix outputs

--- modules/private-tls-cert/outputs.tf
+++ modules/private-tls-cert/outputs.tf
@@ -1,11 +1,11 @@
 output "ca_public_key_file_path" {
-  value = "${var.ca_public_key_file_path}"
+  value = var.ca_public_key_file_path
 }
 
 output "public_key_file_path" {
-  value = "${var.public_key_file_path}"
+  value = var.public_key_file_path
 }
 
 output "private_key_file_path" {
-  value = "${var.private_key_file_path}"
+  value = var.private_key_file_path
 }

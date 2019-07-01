c49d93e5dbbb03cdab3acf51d6a249bf70f69f77
Teodor Linkin <teodor.linkin@technolink.org>
2019-07-01 12:52:01 +0300

    Update main.tf

--- modules/private-tls-cert/main.tf
+++ modules/private-tls-cert/main.tf
@@ -3,22 +3,22 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "tls_private_key" "ca" {
-  algorithm   = "${var.private_key_algorithm}"
-  ecdsa_curve = "${var.private_key_ecdsa_curve}"
-  rsa_bits    = "${var.private_key_rsa_bits}"
+  algorithm   = var.private_key_algorithm
+  ecdsa_curve = var.private_key_ecdsa_curve
+  rsa_bits    = var.private_key_rsa_bits
 }
 
 resource "tls_self_signed_cert" "ca" {
-  key_algorithm     = "${tls_private_key.ca.algorithm}"
-  private_key_pem   = "${tls_private_key.ca.private_key_pem}"
+  key_algorithm     = tls_private_key.ca.algorithm
+  private_key_pem   = tls_private_key.ca.private_key_pem
   is_ca_certificate = true
 
-  validity_period_hours = "${var.validity_period_hours}"
-  allowed_uses          = ["${var.ca_allowed_uses}"]
+  validity_period_hours = var.validity_period_hours}"
+  allowed_uses          = var.ca_allowed_uses
 
   subject {
-    common_name  = "${var.ca_common_name}"
-    organization = "${var.organization_name}"
+    common_name  = var.ca_common_name
+    organization = var.organization_name
   }
 
   # Store the CA public key in a file.
@@ -32,9 +32,9 @@ resource "tls_self_signed_cert" "ca" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "tls_private_key" "cert" {
-  algorithm   = "${var.private_key_algorithm}"
-  ecdsa_curve = "${var.private_key_ecdsa_curve}"
-  rsa_bits    = "${var.private_key_rsa_bits}"
+  algorithm   = var.private_key_algorithm
+  ecdsa_curve = var.private_key_ecdsa_curve
+  rsa_bits    = var.private_key_rsa_bits
 
   # Store the certificate's private key in a file.
   provisioner "local-exec" {
@@ -43,26 +43,26 @@ resource "tls_private_key" "cert" {
 }
 
 resource "tls_cert_request" "cert" {
-  key_algorithm   = "${tls_private_key.cert.algorithm}"
-  private_key_pem = "${tls_private_key.cert.private_key_pem}"
+  key_algorithm   = tls_private_key.cert.algorithm
+  private_key_pem = tls_private_key.cert.private_key_pem
 
-  dns_names    = ["${var.dns_names}"]
-  ip_addresses = ["${var.ip_addresses}"]
+  dns_names    = var.dns_names
+  ip_addresses = var.ip_addresses
 
   subject {
-    common_name  = "${var.common_name}"
-    organization = "${var.organization_name}"
+    common_name  = var.common_name
+    organization = var.organization_name
   }
 }
 
 resource "tls_locally_signed_cert" "cert" {
-  cert_request_pem = "${tls_cert_request.cert.cert_request_pem}"
+  cert_request_pem = tls_cert_request.cert.cert_request_pem
 
-  ca_key_algorithm   = "${tls_private_key.ca.algorithm}"
-  ca_private_key_pem = "${tls_private_key.ca.private_key_pem}"
-  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
+  ca_key_algorithm   = tls_private_key.ca.algorithm
+  ca_private_key_pem = tls_private_key.ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
-  validity_period_hours = "${var.validity_period_hours}"
+  validity_period_hours = var.validity_period_hours
   allowed_uses          = ["${var.allowed_uses}"]
 
   # Store the certificate's public key in a file.

9410447c3500beef28f51a2cb64de6c3ed8af6fc
Yevgeniy Brikman <brikis98@gmail.com>
2020-10-16 10:57:16 +0100

    Require TF 0.12.26 everywhere

--- examples/vault-dynamodb-backend/dynamodb/main.tf
+++ examples/vault-dynamodb-backend/dynamodb/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
 resource "aws_dynamodb_table" "vault_dynamo" {
   name           = var.table_name
   hash_key       = "Path"
